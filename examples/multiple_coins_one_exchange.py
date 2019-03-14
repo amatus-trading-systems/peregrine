4083018b9d098270e76ca8d89fd8e0db7f66664d
Søren Houen <s@houen.net>
2019-03-14 12:39:05 +0100

    WIP

--- examples/multiple_coins_one_exchange.py
+++ examples/multiple_coins_one_exchange.py
@@ -2,7 +2,10 @@ import asyncio
 from peregrinearb import load_exchange_graph, print_profit_opportunity_for_path, bellman_ford
 
 loop = asyncio.get_event_loop()
-graph = loop.run_until_complete(load_exchange_graph('hitbtc'))
-paths = bellman_ford(graph, 'BTC', unique_paths=True)
-for path in paths:
-    print_profit_opportunity_for_path(graph, path)
+for exchange in ['gemini', 'hitbtc', 'binance', 'bittrex', 'huobipro', 'poloniex', 'okex']:
+# for exchange in ['hitbtc', 'okex']:
+    print(exchange.upper())
+    graph = loop.run_until_complete(load_exchange_graph(exchange, fees=True))
+    paths = bellman_ford(graph, 'BTC', unique_paths=True)
+    for path in paths:
+        print_profit_opportunity_for_path(graph, path, starting_amount=100)
